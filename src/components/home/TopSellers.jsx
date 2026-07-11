164952e81dcf4c0d5f79566837255dc27c676c42
Huey T <hueyathotmail@gmail.com>
2026-07-11 07:27:32 -0500

    dynamic top sellers and skeleton loading state

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
+import axios from "axios";
 
 const TopSellers = () => {
+
+  const[loading, setLoading] = useState(false);
+  const[response, setResponse] = useState([]);
+
+  async function getData() {
+    setLoading(true)
+    const res = await axios.get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers`)
+
+    setResponse(res.data || [])
+    setLoading(false);
+  }
+
+  useEffect(()=>{
+    getData();
+  },[])
+
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -15,24 +33,37 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
-                  <div className="author_list_pp">
-                    <Link to="/author">
-                      <img
-                        className="lazy pp-author"
-                        src={AuthorImage}
-                        alt=""
-                      />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
-                  </div>
-                </li>
-              ))}
+              {loading
+                ?
+                  new Array(12).fill(0).map((_, index) => (
+                    <div className="nft__item3 skeleton3" key={index}>
+                      <div className="skeleton-circle3" />
+                      <div className="nft__item_info3">
+                        <div className="skeleton-text3" />
+                        <div className="skeleton-text3 short3" />
+                      </div>
+                    </div>
+                  ))
+                :
+                  response.map((elem, index) => (
+                    <li key={index}>
+                      <div className="author_list_pp">
+                        <Link to="/author">
+                          <img
+                            className="lazy pp-author"
+                            src={elem.authorImage}
+                            alt=""
+                          />
+                          <i className="fa fa-check"></i>
+                        </Link>
+                      </div>
+                      <div className="author_list_info">
+                        <Link to="/author">{elem.authorName}</Link>
+                        <span>{elem.price} ETH</span>
+                      </div>
+                    </li>
+                  ))
+              }
             </ol>
           </div>
         </div>
