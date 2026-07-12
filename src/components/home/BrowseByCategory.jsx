fabdb13cc19de30c502cfb16cc49b294b868e24f
Huey T <hueyathotmail@gmail.com>
2026-07-11 21:53:03 -0500

    hueyt-animation

--- src/components/home/BrowseByCategory.jsx
+++ src/components/home/BrowseByCategory.jsx
@@ -13,37 +13,73 @@ const BrowseByCategory = () => {
             </div>
           </div>
           <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
-            <Link to="/explore" className="icon-box style-2 rounded">
+            <Link to="/explore" className="icon-box style-2 rounded"
+            data-aos="fade-left"
+                  data-aos-delay="100"
+                  data-aos-duration="1000"
+                  data-aos-easing="ease-in-out"
+                  data-aos-mirror="true"
+                  data-aos-once="true">
               <i className="fa fa-image"></i>
               <span>Art</span>
             </Link>
           </div>
           <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
-            <Link to="/explore" className="icon-box style-2 rounded">
+            <Link to="/explore" className="icon-box style-2 rounded"
+            data-aos="fade-left"
+                  data-aos-delay="200"
+                  data-aos-duration="1000"
+                  data-aos-easing="ease-in-out"
+                  data-aos-mirror="true"
+                  data-aos-once="true">
               <i className="fa fa-music"></i>
               <span>Music</span>
             </Link>
           </div>
           <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
-            <Link to="/explore" className="icon-box style-2 rounded">
+            <Link to="/explore" className="icon-box style-2 rounded"
+            data-aos="fade-left"
+                  data-aos-delay="300"
+                  data-aos-duration="1000"
+                  data-aos-easing="ease-in-out"
+                  data-aos-mirror="true"
+                  data-aos-once="true">
               <i className="fa fa-search"></i>
               <span>Domain Names</span>
             </Link>
           </div>
           <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
-            <Link to="/explore" className="icon-box style-2 rounded">
+            <Link to="/explore" className="icon-box style-2 rounded"
+            data-aos="fade-left"
+                  data-aos-delay="400"
+                  data-aos-duration="1000"
+                  data-aos-easing="ease-in-out"
+                  data-aos-mirror="true"
+                  data-aos-once="true">
               <i className="fa fa-globe"></i>
               <span>Virtual Worlds</span>
             </Link>
           </div>
           <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
-            <Link to="/explore" className="icon-box style-2 rounded">
+            <Link to="/explore" className="icon-box style-2 rounded"
+            data-aos="fade-left"
+                  data-aos-delay="500"
+                  data-aos-duration="1000"
+                  data-aos-easing="ease-in-out"
+                  data-aos-mirror="true"
+                  data-aos-once="true">
               <i className="fa fa-vcard"></i>
               <span>Trading Cards</span>
             </Link>
           </div>
           <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
-            <Link to="/explore" className="icon-box style-2 rounded">
+            <Link to="/explore" className="icon-box style-2 rounded"
+            data-aos="fade-left"
+                  data-aos-delay="600"
+                  data-aos-duration="1000"
+                  data-aos-easing="ease-in-out"
+                  data-aos-mirror="true"
+                  data-aos-once="true">
               <i className="fa fa-th"></i>
               <span>Collectibles</span>
             </Link>
